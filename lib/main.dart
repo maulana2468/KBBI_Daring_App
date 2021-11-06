417d4751bc5c919dd8136e20e71ffa0ddcec6a4e
Maulana Yusuf <ymaulana089@gmail.com>
2021-11-06 11:37:46 +0700

    update http

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:kbbi_daring/provider/db_vocab.dart';
@@ -6,12 +8,22 @@ import 'package:kbbi_daring/ui/main_menu.dart';
 import 'package:provider/provider.dart';
 import 'package:path_provider/path_provider.dart' as pathProvider;
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   var appDocumentDirectory =
       await pathProvider.getApplicationDocumentsDirectory();
   Hive.init(appDocumentDirectory.path);
   Hive.registerAdapter(DataVocabAdapter());
+  HttpOverrides.global = MyHttpOverrides();
   runApp(MyApp());
 }
 
